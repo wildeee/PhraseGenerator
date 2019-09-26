c441149db286e8390d5642aacc208bc277f491d8
Wilde <gutopicciani@yahoo.com.br>
2019-09-26 01:17:51 -0300

    Estrutura básica gerada

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <p className="phrase">"Para ter um negócio de sucesso, alguém, algum dia, teve que tomar uma atitude de coragem."</p>
+        <p className="author">Wilde Rossi</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
